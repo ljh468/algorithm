8451e54395b066f3d3b57c819ae8c9ca836b4fd4
ljh468 <ljh468@naver.com>
2021-04-28 00:54:29 +0900

    0428

--- grammar_test03.py
+++ grammar_test03.py
@@ -1,5 +1,7 @@
 # 클래스
-#
+# 하나의 함수를 별개의 객체로 사용하여 여러개를 사용할 수 있음
+# 객체를 이용한 코드는 메서드와 인스턴스 변수가 객체를 이용하지 않고 만든 프로그램보다 안전하게 있다고 보관되어 있기 때문에
+# 다른 코드들의 영향을 받아서 훼손, 변형될 가능성이 현저히 낮음
 
 # 클래스 생성
 class Service:
@@ -86,3 +88,65 @@ juliet = HouseKim("줄리엣")
 juliet.travel("독도")
 
 # 매소드 오버라이딩(Overriding)
+class HouseKim(HousePark2):
+    lastname = "김"
+    def travel(self, where, day):
+        print("%s, %s여행 %d일 가네." %(self.fullname, where, day))
+
+juliet = HouseKim("줄리엣")
+juliet.travel("독도", 3)
+
+# 연산자 오버로딩(Overloading)
+class HousePark:
+    lastname = "박"
+    def __init__(self, name):
+        self.fullname = self.lastname + name
+    def travel(self, where):
+        print("%s, %^s여행을 가다." % (self.fullname, where))
+    def love(self, other):
+        print("%s, %s 사랑에빠졌네" % (self.fullname, other.fullname))
+    def __add__(self, other):
+        print("%s, %s 결혼했네" % (self.fullname, other.fullname))
+
+class HouseKim(HousePark):
+    lastname = "김"
+    def travel(self, where, day):
+        print("%s, %s여행 %d일 가네." % (self.fullname, where, day))
+pey = HousePark("응용")
+juliet = HouseKim("줄리엣")
+pey.love(juliet)
+pey + juliet
+
+
+class HousePark:
+    lastname = "박"
+    def __init__(self, name):
+        self.fullname = self.lastname + name
+    def travel(self, where):
+        print("%s, %s여행을 가다." % (self.fullname, where))
+    def love(self, other):
+        print("%s, %s 사랑에 빠졌네" % (self.fullname, other.fullname))
+    def fight(self, other):
+        print("%s, %s 싸우네" % (self.fullname, other.fullname))
+    def __add__(self, other):
+        print("%s, %s 결혼했네" % (self.fullname, other.fullname))
+
+    def __sub__(self, other):
+        print("%s, %s 이혼했네" % (self.fullname, other.fullname))
+
+
+class HouseKim(HousePark):
+    lastname = "김"
+
+    def travel(self, where, day):
+        print("%s, %s여행 %d일 가네." % (self.fullname, where, day))
+
+
+pey = HousePark("응용")
+juliet = HouseKim("줄리엣")
+pey.travel("부산")
+juliet.travel("부산", 3)
+pey.love(juliet)
+pey + juliet
+pey.fight(juliet)
+pey - juliet
