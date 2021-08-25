160ecbe19b657270055143320d058a4158908b7f
ljh468 <ljh468@naver.com>
2021-08-25 11:52:35 +0900

    이상한문자 만들기

--- python_Programmers/1단계/이상한문자만들기.py
+++ python_Programmers/1단계/이상한문자만들기.py
@@ -1,3 +1,32 @@
+# 내 풀이
 def solution(s):
     answer = ''
+    cnt = 0
+    for word in s:
+        if word == ' ':
+            answer += ' '
+            cnt = 0
+        elif cnt % 2 == 0:
+            answer += word.upper()
+            cnt += 1
+        else:
+            answer += word.lower()
+            cnt += 1
     return answer
+
+# 모범풀이
+def solution2(s):
+    liste = []
+    for i in s.split():
+        result = ''
+        for n, v in enumerate(i):
+            if n % 2 == 0:
+                result += v.upper()
+            else:
+                result += v.lower()
+        liste.append(result)
+    return ' '.join(liste)
+
+s = "try hello world"
+print(solution(s))
+print(solution2(s))
