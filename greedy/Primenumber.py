b6b09aa99c4b03471e8da5c25815d4e9d02657a5
ljh468 <ljh468@naver.com>
2021-09-01 13:18:10 +0900

    소수찾기

--- greedy/Primenumber.py
+++ greedy/Primenumber.py
@@ -1,7 +1,7 @@
 import time
-
+import math
 num = int(input())
-
+#
 ## 소수 판별
 add = []
 start = time.time()
@@ -12,7 +12,7 @@ for i in range(2, num+1):
     else:
         add.append(i)
 
-print(add)
+print(len(add))
 print("time :", time.time() - start)
 
 ## 소수 판별
@@ -50,12 +50,35 @@ for i in range(2, num+1):
 
 print(add2)
 
-## 에라토스테니스의 체
-add3 = [True]*(num+1)
+# 에라토스테니스의 체
+add3 = [True]*(num+1) # 특정 수가 지워졌는지 아닌지 확인하기 위한 배열
 m = int(num**0.5)
 
 for i in range(2,m+1):
-    if add3[i] == True:
-        for j in range(i+i, num+1,i):
+    if add3[i] == True:  # 특정 수가 지워지지 않았다면 (소수여서)
+        for j in range(i+i, num+1, i):
             add3[j]=False
 print([i for i in range(2,num+1) if add3[i]==True])
+
+
+
+# 에라토스테니스의 체 2
+# 소수 판별 함수(에라토스테네스의 체)
+def is_prime_number(n):
+    # 2부터 n까지의 모든 수에 대하여 소수 판별
+    array = [True for i in range(n+1)] # 처음엔 모든 수가 소수(True)인 것으로 초기화(0과 1은 제외)
+
+    # 에라토스테네스의 체
+    for i in range(2, int(math.sqrt(n)) + 1): #2부터 n의 제곱근까지의 모든 수를 확인하며
+        if array[i] == True: # i가 소수인 경우(남은 수인 경우)
+            # i를 제외한 i의 모든 배수를 지우기
+            j = 2
+            while i * j <= n:
+                array[i * j] = False
+                j += 1
+
+    return [ i for i in range(2, n+1) if array[i] ]
+
+# N이 1,000,000 이내로 주어지는 경우 활용할 것 => 이론상 400만번 정도 연산이고 메모리도 충분함
+
+print(is_prime_number(26))
