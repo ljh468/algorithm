2ef6b25e405368d9d3494adf40d348fbc341e0a3
ljh468 <ljh468@naver.com>
2023-07-30 16:45:12 +0900

    선택정렬

---
+++ java/inflearn/src/정렬/_01_선택정렬.java
@@ -0,0 +1,42 @@
+package 정렬;
+
+import java.util.Scanner;
+
+public class _01_선택정렬 {
+
+  public int[] solution(int n, int[] arr) {
+
+    // 1. 선택정렬은 n-1 회전한다.
+    for (int i = 0; i < n - 1; i++) {
+
+      // 2. 제일 작은 수의 위치를 찾는다.
+      int idx = i;
+      for (int j = i + 1; j < n; j++) {
+        if (arr[j] < arr[idx]) {
+          idx = j;
+        }
+      }
+      // 3. 가장 작은 값을 찾아서 탐색의 자리의 수와 바꾼다.
+      int tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+
+    return arr;
+  }
+
+  public static void main(String[] args) {
+    _01_선택정렬 t = new _01_선택정렬();
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    for (int x : t.solution(n, arr)) {
+      System.out.print(x + " ");
+    }
+  }
+}
