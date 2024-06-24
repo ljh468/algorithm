238eeff71098de43360b14d4d2ef428865b05890
ljh468 <ljh468@naver.com>
2024-06-24 13:25:14 +0900

    implement AVLTree insert, remove in java

--- java/data_structure2/src/_AVL트리/AvlTree.java
+++ java/data_structure2/src/_AVL트리/AvlTree.java
@@ -108,6 +108,7 @@ public class AvlTree {
     return targetNode;
   }
 
+  // 균형을 무너뜨리는 노드를 구하는 함수
   public BinaryTree getUnBalanceNode(BinaryTree targetRootNode, BinaryTree unBalanceNode) {
     // 기저조건
     // 해당 노드의 자식노드가 하나도 없을때 함수 종료 (터미널 노드)
@@ -135,4 +136,144 @@ public class AvlTree {
     return unBalanceNode;
   }
 
+  // 노드 삽입(재귀를 이용한 하향식)
+  public BinaryTree insert(BinaryTree targetRootNode, int data) {
+    // 기저조건 (새로운 노드를 만들어줌)
+    if (targetRootNode == null) {
+      targetRootNode = new BinaryTree(data);
+    }
+
+    // AVL 트리에 처음 데이터를 삽입하는 경우
+    if (this.root == null) {
+      this.root = targetRootNode;
+    }
+    // 중복데이터 삽입시 함수 종료
+    else if (targetRootNode.getData() == data) {
+      return targetRootNode;
+    }
+    // 삽입하려는 데이터가 해당 노드보다 작은 경우 처리 (왼쪽으로 재귀)
+    else if (targetRootNode.getData() > data) {
+      // 새로 추가된 노드를 왼쪽 자식노드에 연결
+      targetRootNode.setLeftSubTree(this.insert(targetRootNode.getLeftSubTree(), data));
+    }
+    // 삽입하려는 데이터가 해당 노드보다 큰 경우 처리 (오른쪽 으로 재귀)
+    else {
+      targetRootNode.setRightSubTree(this.insert(targetRootNode.getRightSubTree(), data));
+    }
+
+    // 데이터가 삽입되었으니 높이를 업데이트
+    this.updateHeight(targetRootNode);
+    targetRootNode = this.rotation(targetRootNode, data);
+    return targetRootNode;
+  }
+
+  // 노드 제거 (재귀를 이용한 하향식) - 타겟루트노드를 리턴 (제거에 성공하면 대체될 자식노드를 리턴)
+  public BinaryTree remove(BinaryTree targetRootNode, int data, BinaryTree parentNode) {
+    // 제거할 노드가 타겟루트노드보다 작은경우 왼쪽으로 이동, 타겟루트노드의 왼쪽 자식노드가 존재하면 왼쪽으로 재귀 호출
+    if (targetRootNode.getData() > data && targetRootNode.getLeftSubTree() != null) {
+      targetRootNode.setLeftSubTree(remove(targetRootNode.getLeftSubTree(), data, targetRootNode));
+    }
+    // 제거할 노드가 타겟루트노드보다 큰경우 오른쪽으로 이동, 타겟루트노드의 오른쪽 자식노드가 존재하면 오른쪽으로 재귀 호출
+    else if (targetRootNode.getData() < data && targetRootNode.getRightSubTree() != null) {
+      targetRootNode.setRightSubTree(remove(targetRootNode.getRightSubTree(), data, targetRootNode));
+    }
+    // 제거할 노드를 찾았으면 제거후 대체할 노드를 반환
+    else if (targetRootNode.getData() == data) {
+      targetRootNode = removeHelper(targetRootNode, parentNode);
+      // 루트노드만 제거할 경우에도 높이 업데이트와 회전을 처리할 수 있도록 함
+      if (parentNode == null && targetRootNode != null) {
+        // 제거할 노드의 상위 노드의 높이 업데이트
+        updateHeight(targetRootNode);
+        // 균형을 무너뜨리는 노드를 조회하여, 회전 처리
+        BinaryTree unBalanceNode = getUnBalanceNode(targetRootNode, null);
+        targetRootNode = rotation(targetRootNode, unBalanceNode.getData());
+      }
+      return targetRootNode;
+    }
+
+    // 제거할 노드의 상위 노드의 높이 업데이트
+    updateHeight(targetRootNode);
+    // 균형을 무너뜨리는 노드를 조회하여, 회전 처리
+    BinaryTree unBalanceNode = getUnBalanceNode(targetRootNode, null);
+    targetRootNode = rotation(targetRootNode, unBalanceNode.getData());
+    return targetRootNode;
+  }
+
+  // removeHelper는 노드 제거후 대체할 노드를 반환
+  public BinaryTree removeHelper(BinaryTree deletingNode, BinaryTree parentNode) { // 4, 3
+    BinaryTree fakeParentRootNode = new BinaryTree(0);
+    fakeParentRootNode.setRightSubTree(this.root);
+    
+    if (parentNode == null) {
+      parentNode = fakeParentRootNode;
+    }
+
+    BinaryTree deletingNodeChild = null;
+    if (deletingNode.getLeftSubTree() == null && deletingNode.getRightSubTree() == null) {
+      if (parentNode.getLeftSubTree() == deletingNode) {
+        parentNode.removeLeftSubTree();
+      } else {
+        parentNode.removeRightSubTree();
+      }
+    } else if (deletingNode.getLeftSubTree() == null || deletingNode.getRightSubTree() == null) {
+      if (deletingNode.getLeftSubTree() != null) {
+        deletingNodeChild = deletingNode.getLeftSubTree();
+      } else {
+        deletingNodeChild = deletingNode.getRightSubTree();
+      }
+
+      if (parentNode.getLeftSubTree() == deletingNode) {
+        parentNode.setLeftSubTree(deletingNodeChild);
+      } else {
+        parentNode.setRightSubTree(deletingNodeChild);
+      }
+    } else {
+      // 2-3. 삭제할 노드의 자식노드가 2개일때
+      // 왼쪽 1번 -> 오른쪽 계속 이동
+      BinaryTree replacingNode = deletingNode.getLeftSubTree();
+      BinaryTree replacingNodeParent = deletingNode;
+      while (replacingNode.getRightSubTree() != null) {
+        replacingNodeParent = replacingNode;
+        replacingNode = replacingNode.getRightSubTree();
+      }
+      // 삭제할 노드를 대체할 노드의 값으로 변경
+      deletingNode.setData(replacingNode.getData());
+
+      if (replacingNodeParent.getLeftSubTree() == replacingNode) {
+        replacingNodeParent.setLeftSubTree(replacingNode.getLeftSubTree());
+      } else {
+        replacingNodeParent.setRightSubTree(replacingNode.getLeftSubTree());
+      }
+      deletingNodeChild = deletingNode;
+    }
+
+    if (fakeParentRootNode.getRightSubTree() != this.root) {
+      this.root = fakeParentRootNode.getRightSubTree();
+    }
+    return deletingNodeChild;
+  }
+
+  public static void main(String[] args) {
+    AvlTree avlTree = new AvlTree();
+    System.out.println("========== insert ==========");
+    avlTree.insert(avlTree.root, 1);
+    avlTree.insert(avlTree.root, 2);
+    avlTree.insert(avlTree.root, 3);
+    avlTree.insert(avlTree.root, 4);
+    avlTree.insert(avlTree.root, 5);
+    avlTree.insert(avlTree.root, 6);
+    avlTree.insert(avlTree.root, 7);
+    System.out.println(avlTree.root);
+    avlTree.root.inOrderTraversal(avlTree.root);
+
+    System.out.println("========== remove ==========");
+    avlTree.remove(avlTree.root, 2, null);
+    avlTree.remove(avlTree.root, 3, null);
+    avlTree.remove(avlTree.root, 1, null);
+    System.out.println(avlTree.root);
+    avlTree.root.inOrderTraversal(avlTree.root);
+
+    System.out.println("========== search ==========");
+    System.out.println(avlTree.search(7));
+  }
 }
