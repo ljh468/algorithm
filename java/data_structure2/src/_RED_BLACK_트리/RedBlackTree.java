d22eda3689e850d4d2fb6a93edc47a0851e26ebf
ljh468 <ljh468@naver.com>
2024-06-30 18:37:34 +0900

    implement RedBlackTree remove in java

--- java/data_structure2/src/_RED_BLACK_트리/RedBlackTree.java
+++ java/data_structure2/src/_RED_BLACK_트리/RedBlackTree.java
@@ -24,10 +24,10 @@ public class RedBlackTree {
       // targetData == currentNode.getData()가 같으면 탐색완료
       if (targetData == currentNode.getData()) {
         return currentNode;
-      } else if (targetData > currentNode.getData()) { // targetData가 크면 오른쪽
-        currentNode = currentNode.getRightSubTree();
       } else if (targetData < currentNode.getData()) { // targetData가 작으면 왼쪽
         currentNode = currentNode.getLeftSubTree();
+      } else if (targetData > currentNode.getData()) { // targetData가 크면 오른쪽
+        currentNode = currentNode.getRightSubTree();
       }
     }
     // 만약 찾을 데이터가 없으면 null 리턴
@@ -202,10 +202,196 @@ public class RedBlackTree {
     return node == null || node.isColor() == BLACK;
   }
 
+  public void remove(int data) {
+    BinaryTree currentNode = this.root;
+    // 제거할 노드 탐색
+    while (currentNode != null && currentNode.getData() != data) {
+      if (data < currentNode.getData()) {
+        currentNode = currentNode.getLeftSubTree();
+      } else if (data > currentNode.getData()) {
+        currentNode = currentNode.getRightSubTree();
+      }
+    }
+
+    // 제거할 노드를 찾지 못하면 함수 종료
+    if (currentNode == null) {
+      return;
+    }
+
+    BinaryTree replaceNode = null; // 대체할 노드를 담을 변수
+    boolean deletedNodeColor = RED; // 제거된 노드의 색을 담을 변수
+
+    // 제거할 노드의 자식노드가 0 ~ 1개일때 처리 (즉, 자식노드가 1개 이하라면)
+    if (currentNode.getLeftSubTree() == null || currentNode.getRightSubTree() == null) {
+      replaceNode = this.removeWithZeroOneChild(currentNode);
+      deletedNodeColor = currentNode.isColor();
+    }
+    // 제거할 노드의 자식노드가 2개일때 처리
+    else {
+      // 대체할 노드 탐색 (대체할 노드는 제거하려는 노드의 왼쪽자식노드에서 가장 큰 값)
+      BinaryTree successor = getBiggestNode(currentNode.getLeftSubTree());
+      // 현재노드를 대체할 노드의 값으로 교체
+      currentNode.setData(successor.getData());
+      replaceNode = this.removeWithZeroOneChild(successor); // 가장 큰 노드는 자식노드가 1개 이하임
+      deletedNodeColor = currentNode.isColor();
+    }
+
+    // Red-Black 트리 규칙을 위반했는지 체크하고 처리
+    // 제거했을 때 문제가 되는 노드는 제거한 노드가 검은색인 경우
+    if (deletedNodeColor == BLACK) {
+      // 형제노드를 보면서 5가지 경우를 처리해야함
+      this.rebalanceAfterDeletion(replaceNode);
+      // 대체한 노드를 처리할때, null대신 임시로 NilNode로 연결해서 처리한 경우에 다시 null로 변경
+      if (replaceNode instanceof NilNode) {
+        this.replaceParentsChild(replaceNode.getParentTree(), replaceNode, null);
+      }
+    }
+
+
+  }
+
+  // 삭제후 균형을 맞춰주는 함수 (대체된 노드를 매개변수로 가짐)
+  void rebalanceAfterDeletion(BinaryTree node) {
+    // 대체한 노드가 루트노드인 경우
+    if (node == this.root) {
+      node.setColor(BLACK);
+      return;
+    }
+
+    BinaryTree sibling = getSibling(node);
+    // 1. 형제노드가 빨간색인 경우
+    if (sibling.isColor() == RED) {
+      this.handleRedSibling(node, sibling);
+      // 1번 규칙을 제외한 나머지 규칙을 다시 위반할 수 있음 (회전이 이루어지기 때문)
+      // 다음 단계에서 확인할 수 있도록 형제노드를 업데이트
+      sibling = this.getSibling(node);
+    }
+
+    // 2 ~ 5. 형제노드가 검은색인 경우
+    if (this.isBlack(sibling)) {
+      // 2 ~ 3. 형제노드의 두자식 노드가 모두 검은색인 경우
+      if (isBlack(sibling.getLeftSubTree()) && isBlack(sibling.getRightSubTree())) {
+        // 2. 형제노드와 형제노드의 두자식 노드가 모두 검은색이고, 부모노드가 빨간색인 경우
+        if (node.getParentTree().isColor() == RED) {
+          sibling.setColor(RED); // 형제노드를 빨간색
+          node.getParentTree().setColor(BLACK); // 부모노드를 검은색
+        }
+        // 3. 형제노드와 형제노드의 두자식 노드, 부모노드가 모두 검은색인 경우
+        else {
+          sibling.setColor(RED); // 형제노드를 빨간색
+          // 5번 규칙(모든 경로는 검은색의 갯수가 같음)을 위반하게됨
+          // 균형을 맞출수 있도록 부모노드를 대상으로 재귀호출해줌
+          this.rebalanceAfterDeletion(node.getParentTree());
+        }
+      }
+      // 4 ~ 5. 형제의 두 자식노드중 하나라도 빨간색인 경우
+      else {
+        // 4. 형제노드가 검은색이고 형제노드의 두 자식노드 중 하나라도 빨간색 노드가 있고 "바깥쪽 조카 노드"가 검은색인 경우
+        // 5. 형제노드가 검은색이고 형제노드의 두 자식노드 중 하나라도 빨간색 노드가 있고 "바깥쪽 조카 노드"가 빨간색인 경우
+        this.handleBlackSiblingWithAtLeastOneRedChild(node, sibling);
+      }
+    }
+
+  }
+
+  BinaryTree removeWithZeroOneChild(BinaryTree node) {
+    // 제거할 노드가 왼쪽 자식이 있는 경우
+    if (node.getLeftSubTree() != null) {
+      this.replaceParentsChild(node.getParentTree(), node, node.getLeftSubTree());
+      return node.getLeftSubTree();
+    }
+    // 제거할 노드가 오른쪽 자식이 있는 경우
+    else if (node.getRightSubTree() != null) {
+      this.replaceParentsChild(node.getParentTree(), node, node.getRightSubTree());
+      return node.getRightSubTree();
+    }
+    // 자식노드가 모두 Null인 경우
+    else {
+      // 제거한 노드가 검은색이라면 균형 처리를 해주기위해 NilNode를 연결시켜줌
+      BinaryTree newChild = (node.isColor() == BLACK) ? new NilNode() : null;
+      this.replaceParentsChild(node.getParentTree(), node, newChild);
+      return newChild;
+    }
+  }
+
+  BinaryTree getBiggestNode(BinaryTree node) {
+    while (node.getRightSubTree() != null) {
+      node = node.getRightSubTree();
+    }
+    return node;
+  }
+
+  // 형제노드를 구하는 함수
+  BinaryTree getSibling(BinaryTree node) {
+    BinaryTree parentNode = node.getParentTree();
+    if (node == parentNode.getLeftSubTree()) {
+      return parentNode.getRightSubTree();
+    } else {
+      return parentNode.getLeftSubTree();
+    }
+  }
+
+  // 균형을 잡을때 형제노드가 빨간색인 경우에 처리하는 함수
+  void handleRedSibling(BinaryTree node, BinaryTree sibling) {
+    sibling.setColor(BLACK); // 형제노드 검은색으로 변경
+    node.getParentTree().setColor(RED); // 부모노드 빨간색으로 변경
+    // 부모노드를 대체된 노드의 방향으로 회전
+    if (node.getParentTree().getLeftSubTree() == node) {
+      this.rotateLeft(node.getParentTree());
+    } else {
+      this.rotateRight(node.getRightSubTree());
+    }
+  }
+
+  // 균형을 잡을때 형제노드가 검은색이고, 형제의 두 자식노드중 하나라도 빨간색인 경우 처리하는 함수
+  void handleBlackSiblingWithAtLeastOneRedChild(BinaryTree node, BinaryTree sibling) {
+    // 대체되는 노드가 부모노드의 왼쪽 자식노드인지 체크
+    boolean nodeIsLeftChild = (node.getParentTree().getLeftSubTree() == node);
+
+    // 4. 형제노드가 검은색이고 형제노드의 두 자식노드 중 하나라도 빨간색 노드가 있고 "바깥쪽 조카 노드"가 검은색인 경우
+    // 4-1. 대체되는 노드가 부모노드의 왼쪽 자식노드이고, 형제노드의 오른쪽 자식노드(null)가 검은색인 조건
+    if (nodeIsLeftChild && isBlack(sibling.getRightSubTree())) {
+      // 안쪽 조카노드를 검은색으로 변경
+      sibling.getLeftSubTree().setColor(BLACK);
+      // 형제노드를 빨간색으로 변경, 그리고 형제노드를 RR 회전
+      sibling.setColor(RED);
+      this.rotateRight(sibling);
+      // 오른쪽으로 쭉 뻗은 구조가 됨 (5번 처리 경우이기때문에 형제노드를 업데이트)
+      sibling = node.getParentTree().getRightSubTree();
+    }
+    // 4-2. 대체되는 노드가 부모노드의 오른쪽 자식노드이고, 형제노드의 왼쪽 자식노드(null)가 검은색인 조건
+    else if (!nodeIsLeftChild && isBlack(sibling.getLeftSubTree())) {
+      sibling.getRightSubTree().setColor(BLACK);
+      sibling.setColor(RED);
+      this.rotateLeft(sibling);
+      sibling = node.getParentTree().getLeftSubTree();
+    }
+
+    // 5. 형제노드가 검은색이고 형제노드의 두 자식노드 중 하나라도 빨간색 노드가 있고 "바깥쪽 조카 노드"가 빨간색인 경우
+    // (4번 로직이 처리된 이후의 경우에도 해당)
+
+    // 형제노드의 색을 부모노드의 색으로 변경
+    sibling.setColor(node.getParentTree().isColor());
+    // 부모노드의 색을 검은색으로 변경
+    node.getParentTree().setColor(BLACK);
+    // 대체되는 노드가 부모노드의 왼쪽 자식노드인 경우 (즉, 오른쪽으로 뻗은 트리)
+    if (nodeIsLeftChild) {
+      // 형제노드의 오른쪽 자식노드를 검은색으로 변경
+      sibling.getRightSubTree().setColor(BLACK);
+      // 부모노드를 제거한 노드방향으로 회전 (LL회전)
+      rotateLeft(node.getParentTree());
+    } else {
+      // 형제노드의 왼쪽 자식노드를 검은색으로 변경
+      sibling.getLeftSubTree().setColor(BLACK);
+      // 부모노드를 제거한 노드방향으로 회전 (RR회전)
+      rotateRight(node.getParentTree());
+    }
+  }
+
   public static void main(String[] args) {
     RedBlackTree redBlackTree = new RedBlackTree();
 
-    System.out.println("========== insert ==========");
+    System.out.println("========== insert 17, 9, 19, 75, 85 ==========");
     redBlackTree.insert(17);
     redBlackTree.insert(9);
     redBlackTree.insert(19);
@@ -213,10 +399,26 @@ public class RedBlackTree {
     redBlackTree.insert(85);
 
     System.out.println("ROOT NODE = " + redBlackTree.getRoot());
+    System.out.println("17 NODE = " + redBlackTree.search(17));
     System.out.println("9 NODE = " + redBlackTree.search(9));
     System.out.println("19 NODE = " + redBlackTree.search(19));
     System.out.println("75 NODE = " + redBlackTree.search(75));
     System.out.println("85 NODE = " + redBlackTree.search(85));
+
+    System.out.println("========== 중위순회 ==========");
+    redBlackTree.getRoot().inOrderTraversal(redBlackTree.getRoot());
+
+    System.out.println("========== remove 19, 75, 85 ==========");
+    redBlackTree.remove(19);
+    redBlackTree.remove(75);
+    redBlackTree.remove(85);
+    System.out.println("ROOT NODE = " + redBlackTree.getRoot());
+    System.out.println("17 NODE = " + redBlackTree.search(17));
+    System.out.println("9 NODE = " + redBlackTree.search(9));
+    System.out.println("19 NODE = " + redBlackTree.search(19));
+    System.out.println("75 NODE = " + redBlackTree.search(75));
+    System.out.println("85 NODE = " + redBlackTree.search(85));
+
     System.out.println("========== 중위순회 ==========");
     redBlackTree.getRoot().inOrderTraversal(redBlackTree.getRoot());
   }
