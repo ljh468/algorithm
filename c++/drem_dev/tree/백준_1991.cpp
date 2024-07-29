1a2df6dc82e29e0b722f150e18f3a4fd9f32019c
ljh468 <ljh468@naver.com>
2024-07-29 19:06:17 +0900

    BOJ_1991 problem solving with c++

---
+++ c++/drem_dev/tree/백준_1991.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    char left;
+    char right;
+};
+
+// 전위 순회: 루트 -> 왼쪽 -> 오른쪽
+void preorder(Node tree[], char node) {
+    if (node == '.') return;
+    cout << node;  // 루트 노드 출력
+    preorder(tree, tree[node - 'A'].left);  // 왼쪽 서브트리 탐색
+    preorder(tree, tree[node - 'A'].right); // 오른쪽 서브트리 탐색
+}
+
+// 중위 순회: 왼쪽 -> 루트 -> 오른쪽
+void inorder(Node tree[], char node) {
+    if (node == '.') return;
+    inorder(tree, tree[node - 'A'].left);   // 왼쪽 서브트리 탐색
+    cout << node;  // 루트 노드 출력
+    inorder(tree, tree[node - 'A'].right);  // 오른쪽 서브트리 탐색
+}
+
+// 후위 순회: 왼쪽 -> 오른쪽 -> 루트
+void postorder(Node tree[], char node) {
+    if (node == '.') return;
+    postorder(tree, tree[node - 'A'].left); // 왼쪽 서브트리 탐색
+    postorder(tree, tree[node - 'A'].right);// 오른쪽 서브트리 탐색
+    cout << node;  // 루트 노드 출력
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    Node tree[26];  // 최대 26개의 노드 (A-Z)
+
+    for (int i = 0; i < N; ++i) {
+        char parent, left, right;
+        cin >> parent >> left >> right;
+        tree[parent - 'A'] = {left, right};
+    }
+
+    // 전위 순회 결과 출력
+    preorder(tree, 'A');
+    cout << endl;
+
+    // 중위 순회 결과 출력
+    inorder(tree, 'A');
+    cout << endl;
+
+    // 후위 순회 결과 출력
+    postorder(tree, 'A');
+    cout << endl;
+
+    return 0;
+}
