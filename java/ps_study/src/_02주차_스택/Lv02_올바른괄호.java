c7ff633ca48f5045941b0345e1ae5cb6dc447fdf
ljh468 <ljh468@naver.com>
2024-08-16 00:59:31 +0900

    PS programmers Lv02_올바른괄호

---
+++ java/ps_study/src/_02주차_스택/Lv02_올바른괄호.java
@@ -0,0 +1,33 @@
+package _02주차_스택;
+
+import java.util.Stack;
+
+public class Lv02_올바른괄호 {
+
+  boolean solution(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (char ch : s.toCharArray()) {
+      // 만약 여는괄호면 stack에 추가
+      if (ch == '(') {
+        stack.push(ch);
+      }
+      // 만약 닫는괄호면 stack에 여는괄호가 있는지 확인
+      else {
+        // 스택이 비어있거나, 여는괄호가 없다면 올바르지않은 괄호
+        if (stack.isEmpty() || stack.pop() == ')') {
+          return false;
+        }
+      }
+    }
+    // 문자열을 다돌았는데, stack에 데이터가 남아있으면 안됨 (비어있으면 올바른괄호)
+    return stack.isEmpty();
+  }
+
+  public static void main(String[] args) {
+    Lv02_올바른괄호 main = new Lv02_올바른괄호();
+    String str1 = "(())()";
+    String str2 = ")()(";
+    System.out.println("result1 = " + main.solution(str1));
+    System.out.println("result1 = " + main.solution(str2));
+  }
+}
