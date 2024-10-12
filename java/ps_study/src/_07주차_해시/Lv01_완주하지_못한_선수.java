006b3666931843fa0e90c61e1a45b38822cd6ccc
ljh468 <ljh468@naver.com>
2024-10-12 09:29:46 +0900

    class name modify with hash problem

--- java/ps_study/src/_07주차_해시/Lv01_완주하지_못한_선수.java
+++ java/ps_study/src/_07주차_해시/Lv01_완주하지_못한_선수.java
@@ -1,5 +1,5 @@
 package _07주차_해시;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42576
-public class 완주하지_못한_선수 {
+public class Lv01_완주하지_못한_선수 {
 }
--- java/ps_study/src/_07주차_해시/Lv02_의상.java
+++ java/ps_study/src/_07주차_해시/Lv02_의상.java
@@ -1,5 +1,5 @@
 package _07주차_해시;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42578
-public class 의상 {
+public class Lv02_의상 {
 }
