c2f06f7842d4793cb760a07ea61494a374180fcf
ljh468 <ljh468@naver.com>
2024-10-13 23:26:21 +0900

    recursion modify

--- java/data_structure/src/_재귀/LengthOfStr.java
+++ java/data_structure/src/_재귀/LengthOfStr.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class LengthOfStr {
 
   public int strLength(String[] arr) {
-    // 배열의 첫 번째 요소가 null이면 0을 반환
+    // 배열이 비어 있으면 0을 반환
     if (arr.length == 0) {
       return 0;
     }
     // 재귀적으로 배열의 마지막 요소를 제외한 새로운 배열에 +1을 더하여 반환
-    return strLength(Arrays.copyOfRange(arr, 0, arr.length - 1)) + 1;
+    return 1 + strLength(Arrays.copyOfRange(arr, 0, arr.length - 1));
   }
 
   public static void main(String[] args) {
