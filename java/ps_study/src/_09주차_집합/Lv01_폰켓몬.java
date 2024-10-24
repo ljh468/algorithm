179384b8e145e89be62056a6a56b4b4965ef1a8a
ljh468 <ljh468@naver.com>
2024-10-24 18:11:46 +0900

    solving programmers problem [Lv01_폰켓몬] with set

---
+++ java/ps_study/src/_09주차_집합/Lv01_폰켓몬.java
@@ -0,0 +1,27 @@
+package _09주차_집합;
+
+import java.util.HashSet;
+
+public class Lv01_폰켓몬 {
+
+  public static int solution(int[] nums) {
+    // 1. 받을 수 있는 폰켓몬의 수
+    int count = nums.length / 2;
+
+    // 2. nums를 순회하면서 set에 데이터를 추가
+    HashSet<Integer> mon = new HashSet<>();
+    for (int num : nums) {
+      mon.add(num);
+    }
+
+    // 3. 고유한 폰켓몬 종류의 수와 count 중 작은 값을 반환
+    return (Math.min(count, mon.size()));
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {3, 3, 3, 2, 2, 4};
+
+    int result = solution(nums);
+    System.out.println("result = " + result);
+  }
+}
